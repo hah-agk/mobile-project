b5bd730c267b7a674d5e0a04085bd4236251f402
Me-1-afk <hadi.krayem654321@gmail.com>
2025-12-07 01:48:43 +0200

    add bottom nav - hadyy

--- app/src/main/java/com/example/currencyconverter/MainActivity2.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity2.java
@@ -1,77 +1,73 @@
 package com.example.currencyconverter;
 
+import static com.example.currencyconverter.R.id.*;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.currencyconverter.databinding.ActivityMain2Binding;
 import com.example.currencyconverter.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-    TextInputEditText txtUsername, txtEmail, txtCountry;
-    ImageView profileImage;
-    MaterialToolbar toolbar;
+    ActivityMain2Binding binding;
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
 
-    private static final int PICK_IMAGE = 100;
+            super.onCreate(savedInstanceState);
+            binding = ActivityMain2Binding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+            replaceFragment(new HomeFragment());
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+            binding.bottomNavigationBar.setOnItemSelectedListener(
+                    new NavigationBarView.OnItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(@NonNull android.view.MenuItem item) {
 
+                            int id = item.getItemId();
 
+                            if (id == R.id.home) {
+                                replaceFragment(new HomeFragment());
+                                return true;
+                            } else if (id == R.id.profile) {
+                                replaceFragment(new ProfileFragment());
+                                return true;
+                            } else if (id == R.id.chat) {
+                                replaceFragment(new ChatFragment());
+                                return true;
+                            }
 
+                            return false;
+                        }
+                    }
+            );
+        }
 
-//        txtUsername = findViewById(R.id.txtUsername);
-//        txtEmail = findViewById(R.id.txtEmail);
-//        txtCountry = findViewById(R.id.txtCountry);
-//        profileImage = findViewById(R.id.profileImage);
-//        toolbar = findViewById(R.id.profileToolbar);
-//
-//        toolbar.setNavigationOnClickListener(v -> finish());
-//
-//        String user = getIntent().getStringExtra("username");
-//        String email = getIntent().getStringExtra("email");
-//        String country = getIntent().getStringExtra("country");
-//
-//        SharedPreferences prefs = getSharedPreferences("userData", MODE_PRIVATE);
-//
-//        txtUsername.setText(user != null ? user : prefs.getString("username", ""));
-//        txtEmail.setText(email != null ? email : prefs.getString("email", ""));
-//        txtCountry.setText(country != null ? country : prefs.getString("country", ""));
-//
-//        // Clicking Edit Profile → open gallery
-//        findViewById(R.id.btnEdit).setOnClickListener(v -> openGallery());
-//    }
-//
-//    private void openGallery() {
-//        Intent intent = new Intent(Intent.ACTION_PICK);
-//        intent.setType("image/*");
-//        startActivityForResult(intent, PICK_IMAGE);
-//    }
-//
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null) {
-//            Uri imageUri = data.getData();
-//            profileImage.setImageURI(imageUri);
-//        }
+        private void replaceFragment(Fragment fragment){
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frame_layout,fragment);
+            fragmentTransaction.commit();
         }
     }
