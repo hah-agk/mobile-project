26d5a2b16fca8f94866a67bef7f60d5021502246
Me-1-afk <hadi.krayem654321@gmail.com>
2025-12-16 00:41:13 +0200

    app version 2.0 - hadyy

--- app/src/main/java/com/example/currencyconverter/MainActivity2.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity2.java
@@ -1,39 +1,15 @@
 package com.example.currencyconverter;
 
-import static com.example.currencyconverter.R.id.*;
-
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.currencyconverter.data.SharedViewModel;
 import com.example.currencyconverter.databinding.ActivityMain2Binding;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -41,7 +17,6 @@ public class MainActivity2 extends AppCompatActivity {
 //    FirebaseAuth fAuth;
 //    FirebaseFirestore fStore;
 //    String userID;
-    SharedViewModel viewModel;
 
 
         @Override
